5e3eb75c28b449b7f0e9aa7a4263608d8ef2d2eb
edmondgaks <edmondgaks@gmail.com>
2024-01-16 09:46:28 +0200

    Adding the socials logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Moon, Sun } from 'lucide-react';
+import { Dribbble, Facebook, Instagram, Moon, Sun, Twitter } from 'lucide-react';
 import './App.css';
 import { useState } from 'react';
 
@@ -38,8 +38,22 @@ function App() {
             color: "transparent",
           }}>I am an artist of year</h1>
           <p className="font-sans w-[70%] text-sm text-[#C7C5C2] font-light">Blank sheets become the liminal spaces where thoughts are given volume and emotions are rendered in a spectrum of colors that echo the complexity of feeling and the resonance of mental states.</p>
-          <div className="w-full flex flex-row justify-between items-center">
-            <div className="py-3 px-4 rounded-sm text-xs bg-[#FFBA00] text-black">Contact me</div>
+          <div className="w-[60%] flex flex-row justify-between items-center">
+            <div className="py-4 px-6 font-semibold rounded-sm text-xs bg-[#FFBA00] text-black">Contact me</div>
+            <div className="flex flex-row gap-2 items-center">
+              <div className="w-8 h-8 flex justify-center items-center border border-white rounded-full">
+              <Instagram size={16} />
+              </div>
+              <div className="w-8 h-8 flex justify-center items-center border border-white rounded-full">
+              <Twitter size={16} />
+              </div>
+              <div className="w-8 h-8 flex justify-center items-center border border-white rounded-full">
+              <Dribbble size={16} />
+              </div>
+              <div className="w-8 h-8 flex justify-center items-center border border-white rounded-full">
+              <Facebook size={16} />
+              </div>
+            </div>
           </div>
         </div>
         <div className="">
