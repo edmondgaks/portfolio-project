12d690f028a78bc41ee50cd7398fa90af4ebf5d5
edmondgaks <edmondgaks@gmail.com>
2024-01-21 21:31:36 +0200

    optimizing pages and also images

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,6 @@ function App() {
       </div>
       <div className={`flex h-[90vh] w-full flex-row items-center justify-center ${isDarkMode ? "dark-bg" : "bg-[#FFFDFB]"}`}>
         <div className="flex w-[45%] relative flex-col gap-1">
-          <img src="/stars.png" className="absolute bottom-44 -translate-x-9" width={50}  />
           <h1 className="text-5xl font-semibold text-white font-sans" style={{
             backgroundImage: "linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 120%)",
             WebkitBackgroundClip: "text",
@@ -66,18 +65,19 @@ function App() {
           </div>
         </div>
         <div className="">
-          <img src="/Rectangle 1.png" className="" width={500}  />
+          <img src="/madoc.png" className="" width={490}  />
         </div>
       </div>
       <div className={`flex gap-4 h-[100vh] flex-col justify-around items-center`}>
         <h1 className="perpetua text-3xl font-medium">ABOUT <span className="text-[#FFBA00]">MADOC PIERCE</span></h1>
-        <p className="font-sans text-center w-[70%] text-sm text-[#C7C5C2] font-light">I am driven by the pursuit of the intangible, conjuring forth the	abstracted realities that dwell just beyond the periphery of perception. My art is 	an invitation to viewers: to delve into the visual manifestations of mindscapes, to 	grapple with the intangible, and to find solace in the shared quest to understand the immeasurable dimensions of being.</p>
+        <p className="font-sans text-center w-[65%] text-sm text-[#C7C5C2] font-light">I am driven by the pursuit of the intangible, conjuring forth the abstracted realities that dwell just beyond the periphery of perception. My art is 	an invitation to viewers: to delve into the visual manifestations of mindscapes, to grapple with the intangible, and to find solace in the shared quest to understand the immeasurable dimensions of being.</p>
+        <p className="font-sans text-center w-[65%] text-sm text-[#C7C5C2] font-light">A pixelated hive mind becomes my interlocutor and amidst the clash of our two dimensions, previously 	hidden form is discovered and extracted in ways that almost seem impossible. By merging and manipulating these buzzing bio-technological landscapes within the 	digital realm, I compose a symphony of forms that blur the lines between the virtual and the real, the conscious and the subconscious.</p>
         <motion.div ref={carousel} className="carousel">
           <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className="inner-carousel">
             {images.map((image) => {
               return (
                 <motion.div className="item">
-                  <img src={image} alt="" />
+                  <img src={image} alt="" loading="lazy" />
                 </motion.div>
               )
             })}
@@ -103,6 +103,30 @@ function App() {
             <img src="/Bio-architecture no.4.jpg" />
             <img src="/Chromatic Cortex no.2.jpg" />
             <img src="/Lungs.jpg" />
+          </div>
+        </div>
+      </div>
+      <div className={`flex gap-4 mt-10 flex-col justify-around items-center`}>
+        <h1 className="perpetua text-3xl font-medium">CONTACT <span className="text-[#ffba00]">ME</span></h1>
+        <p className="font-sans text-center w-[70%] text-sm text-[#C7C5C2] placeholder:text-xs font-light">My art is an invitation to viewers: to delve into the visual manifestations of mindscapes, to grapple with the intangible, and to find solace in the shared quest to understand the immeasurable dimensions of being.</p>
+        <div className="flex flex-row items-center gap-10">
+          <div className="flex flex-col">
+            <p className="text-sm text-[#FFBA00] font-medium font-sans">Fullname:</p>
+            <input type="text" placeholder="John Doe |" className="p-3 w-[40vh] placeholder:text-xs border-b-[1px] text-[#646363] border-[#858585] bg-[#060503]" />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-sm text-[#FFBA00] font-medium font-sans">Email:</p>
+            <input type="text" placeholder="example@gmail.com" className="p-3 placeholder:text-xs placeholder:text-[#646363] w-[40vh] border-b-[1px] border-[#858585] bg-[#150F02]" />
+          </div>
+        </div>
+        <div className="flex flex-row items-center gap-10">
+          <div className="flex flex-col">
+            <p className="text-sm text-[#FFBA00] font-medium font-sans">Contact Phone:</p>
+            <input type="text" placeholder="eg: 1123-419-345" className="p-3 placeholder:text-xs w-[40vh] border-b-[1px] placeholder:text-[#646363] border-[#858585] bg-[#060503]" />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-sm text-[#FFBA00] font-medium font-sans">City/Town:</p>
+            <input type="text" placeholder="eg: California" className="p-3 placeholder:text-xs placeholder:text-[#646363] w-[40vh] border-b-[1px] border-[#858585] bg-[#150F02]" />
           </div>
         </div>
       </div>
