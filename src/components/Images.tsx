cba1685ad8bcd192327e0cccb0e1528a64c1d170
edmondgaks <edmondgaks@gmail.com>
2024-01-24 15:19:03 +0200

    refactoring pathways

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,22 +1,22 @@
-import img1 from "../../public/az.png";
-import img2 from "../../public/fd.png";
-import img3 from "../../public/gg.png";
-import img4 from "../../public/ff.png";
-import img5 from "../../public/we.png";
-import img6 from "../../public/Arabelle.jpg";
-import img7 from "../../public/Bio-architecture no.4.jpg";
-import img8 from "../../public/Bio-architecture no.7.jpg";
-import img9 from "../../public/Consciousness no.1.jpg";
-import img10 from "../../public/Lungs.jpg";
-import img11 from "../../public/Chromatic Cortex no.2.jpg";
-import img13 from "../../public/Pathways no.5.jpg";
-import img14 from "../../public/Society no.2.jpg";
-import img15 from "../../public/Tongue.jpg";
-import img16 from "../../public/Transcendence no.1.jpg";
-import img17 from "../../public/XMachine no.1.jpg";
-import img18 from "../../public/Unity no.2.jpg";
-import img19 from "../../public/Bio-architecture no.8.jpg";
-import img20 from "../../public/XMachine no.9.jpg";
+import img1 from "/az.png";
+import img2 from "/fd.png";
+import img3 from "/gg.png";
+import img4 from "/ff.png";
+import img5 from "/we.png";
+import img6 from "/Arabelle.jpg";
+import img7 from "/Bio-architecture no.4.jpg";
+import img8 from "/Bio-architecture no.7.jpg";
+import img9 from "/Consciousness no.1.jpg";
+import img10 from "/Lungs.jpg";
+import img11 from "/Chromatic Cortex no.2.jpg";
+import img13 from "/Pathways no.5.jpg";
+import img14 from "/Society no.2.jpg";
+import img15 from "/Tongue.jpg";
+import img16 from "/Transcendence no.1.jpg";
+import img17 from "/XMachine no.1.jpg";
+import img18 from "/Unity no.2.jpg";
+import img19 from "/Bio-architecture no.8.jpg";
+import img20 from "/XMachine no.9.jpg";
 
 
 
