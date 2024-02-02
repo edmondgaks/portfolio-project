7247f8d3379593150a78598b7ae6710112476a23
edmondgaks <edmondgaks@gmail.com>
2024-02-02 11:23:13 +0200

    solving errors

--- src/pages/SingleImage.tsx
+++ src/pages/SingleImage.tsx
@@ -1,5 +1,5 @@
-import { FC, useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { FC } from 'react';
+import { useLocation } from 'react-router-dom';
 
 interface SingleImagePageProps {
 }
@@ -7,14 +7,12 @@ interface SingleImagePageProps {
 const SingleImage: FC<SingleImagePageProps> = () => {
     const location = useLocation();
     const imageSrc = location.state?.image;
-
-    useEffect(() => {
-        // Do any additional setup or fetch data based on the imageSrc if needed
-      }, [imageSrc]);
   return (
-    <div>
+    <div className="w-full h-[100vh] flex flex-col justify-center items-center">
       <h2>Single Image Page</h2>
-      {imageSrc && <img src={imageSrc} alt="Single Image" />}
+      <div className="max-h-[80vh]">
+        {imageSrc && <img src={imageSrc} alt="Single Image" className="w-full h-[100%]" />}
+      </div>
     </div>
   );
 };
